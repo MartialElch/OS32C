b2f9b7631db081038545efb129c3e7eb343fa16e
Stefan Reimbold <Stefan.Reimbold@de.ibm.com>
2023-10-13 10:30:15 +0200

    fix kernel.c

--- kernel.c
+++ kernel.c
@@ -8,6 +8,15 @@
 #define COLOR	0x0a
 
 /******************************************************************************/
+size_t terminal_row;
+size_t terminal_column;
+uint8_t terminal_color;
+
+volatile uint16_t* terminal_buffer;
+
+volatile uint16_t VGA_BUFFER[VGA_WIDTH*VGA_HEIGHT];
+
+/******************************************************************************/
 void terminal_writestring(const char*);
 
 /******************************************************************************/
@@ -19,12 +28,48 @@ void kmain(void) {
 }
 
 /******************************************************************************/
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
+	return (uint16_t) uc | (uint16_t) color << 8;
+}
+
 size_t strlen(const char* str) {
 	size_t len = 0;
 	while (str[len]) {
 		len++;
 	}
 	return len;
+}
+
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = vga_entry(c, color);
+
+	VGA_BUFFER[index] = vga_entry(c, color);
+}
+
+void terminal_refresh(void) {
+	size_t index;
+
+	for (int i=0; i<VGA_HEIGHT; i++) {
+		for (int j=0; j<VGA_WIDTH; j++) {
+			index = i * VGA_WIDTH + j;
+			terminal_buffer[index] = VGA_BUFFER[index];
+		}
+	}
+}
+
+void terminal_scroll(void) {
+	for (int i=0; i<VGA_HEIGHT-1; i++) {
+		for (int j=0; j<VGA_WIDTH; j++) {
+			VGA_BUFFER[i*VGA_WIDTH + j] = VGA_BUFFER[(i+1)*VGA_WIDTH + j];
+		}
+	}
+
+	for (int i=0; i<VGA_WIDTH; i++) {
+		VGA_BUFFER[(VGA_HEIGHT-1)*VGA_WIDTH + i] = vga_entry(' ', terminal_color);
+	}
+
+	terminal_refresh();
 }
 
 void terminal_putchar(char c) {
