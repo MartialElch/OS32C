005fad9c5b4d784c7ef44aabc66eec6837da820f
Stefan Reimbold <Stefan.Reimbold@de.ibm.com>
2023-10-13 17:08:59 +0200

    add keyboard buffer

---
+++ include/keyboard.h
@@ -0,0 +1,9 @@
+#ifndef _IRQ_H
+#define _IRQ_H
+
+struct KeyElement {
+	uint8_t code;
+	char    c;
+};
+
+#endif
---
+++ keyboard.c
@@ -0,0 +1,15 @@
+#include "keyboard.h"
+
+struct KeyElement keybuffer;
+
+void keybuffer_add(uint_8 k) {
+	return;
+}
+
+void keybuffer_init(void) {
+	return;
+}
+
+char keybuffer_read() {
+	return 'a';
+}
