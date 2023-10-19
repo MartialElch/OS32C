cda171e2344c2a907ce8181c7382eb9fbac8099e
Stefan Reimbold <Stefan.Reimbold@de.ibm.com>
2023-10-19 15:13:34 +0200

    implement keymap

--- keyboard.c
+++ keyboard.c
@@ -1,12 +1,28 @@
 #include "keyboard.h"
 
-struct KeyElement keybuffer;
+uint8_t keybuffer[256];
+uint8_t keypos;
+
+keyboard_getcharacter
+
+uint8_t keyboard_getcharacter(uint8_t keycode) {
+	return 'a';
+}
 
 void keybuffer_add(uint8_t keycode) {
+	keybuffer[keypos] = keyboard_getcharacter(keycode);
+	keypos = keypos + 1;
+	keybuffer[keypos] = '\0';
+
+	terminal_writestring("keybuffer\n");
+
 	return;
 }
 
 void keybuffer_init(void) {
+	keypos = 0;
+	keybuffer[keypos] = '\0';
+
 	return;
 }
 
